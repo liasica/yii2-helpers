a072d72f070465a76f1dc21b79616f55f93d44b3
liasica <magicrolan@qq.com>
2016-01-05 13:01:36 +0800

    数组转为简单XML array convert to simple xml

---
+++ SimpleArrayToXml.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Author: liasica
+ * CreateTime: 16/1/5 13:00
+ * Filename: SimpleArrayToXml.php
+ * PhpStorm: yii2-helpers
+ */
+namespace liasica\helpers;
+class SimpleArrayToXml
+{
+  /**
+   * 数组转为简单XML
+   * array convert to simple xml
+   * SimpleArrayToXml constructor.
+   *
+   * @param $arr
+   */
+  public function __construct($arr)
+  {
+    $xml = "<xml>";
+    foreach ($arr as $key => $val)
+    {
+      if (is_numeric($val))
+      {
+        $xml .= "<" . $key . ">" . $val . "</" . $key . ">";
+      }
+      else
+      {
+        $xml .= "<" . $key . "><![CDATA[" . $val . "]]></" . $key . ">";
+      }
+      //$xml .= "<" . $key . ">" . $val . "</" . $key . ">";
+    }
+    $xml .= "</xml>";
+
+    return $xml;
+  }
+}
